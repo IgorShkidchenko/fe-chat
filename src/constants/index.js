3307b2ea4b0b56cf4770ea79f55632d531a2ebd7
Igor Shkidchenko <igorshkidchenko@gmail.com>
2020-06-25 15:13:12 +0300

    create fe chat

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_ROOT = 'http://localhost:3007';
+export const API_WS_ROOT = 'ws://localhost:3007/cable';
+export const HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
